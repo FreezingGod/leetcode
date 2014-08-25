2c8ce5c66c5fd7bd919ebbb9191c50256fe88f4c
Zachary Ling <zachary@Zacharys-MacBook-Air.local>
2014-08-25 11:50:14 +0800

    word break2, sort list in cpp

---
+++ sort_list/sol.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <stddef.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+        if (head == NULL || head->next == NULL) 
+            return head;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while (fast->next != NULL && fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode *head1 = head;
+        ListNode *head2 = slow->next;
+        ListNode *h1 = sortList(head1);
+        ListNode *h2 = sortList(head2);
+        ListNode *rtn = merge(h1, h2);
+        return rtn;
+    }
+    
+    ListNode *merge(ListNode *head1, ListNode *head2) {
+        if (head1 == NULL)
+            return head2;
+        if (head2 == NULL)
+            return head1;
+        ListNode *dummy = new ListNode(0);
+        ListNode *last = dummy;
+        while (head1 != NULL && head2 != NULL) {
+            if (head1->val < head2->val) {
+                last->next = head1;
+                head1 = head1->next;
+                last = last->next;
+            } else {
+                last->next = head2;
+                head2 = head2->next;
+                last = last->next;
+            }
+        }
+        if (head1 == NULL)
+            last->next = head2;
+        if (head2 == NULL)
+            last->next = head1;
+        return dummy->next;
+    }
+};
+
+int main(void) {
+    ListNode* node1 = new ListNode(1);
+    ListNode* node2 = new ListNode(2);
+    node2->next = node1;
+    Solution* sol = new Solution();
+    sol->sortList(node2);
+}
